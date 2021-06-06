aaf8b84a8b7ca05d4b0882a472fcb79cccffb190
narayanpatidar870 <85432429+narayanpatidar870@users.noreply.github.com>
2021-06-06 18:42:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import Header from "./Header";
+import Footer from "./Footer";
+function App(){
+	return (
+		<div>
+		<Header />
+		<footer />
+		</div>
+	);
+}
+export default App;
---
+++ Footer.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import Header from "./Header";
+function Footer(){
+	return (
+		<footer>
+		<p> Copyright @ 2021</p1>
+		</footer>;
+	);
+}
+export default Footer;
